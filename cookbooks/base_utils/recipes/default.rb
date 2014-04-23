2a6e77f8ad19c975e58a489ae49b864305034e40
Frido Koch <info@fridokoch.de>
2014-04-23 20:05:35 +0200

    Fixed Key installtion

--- cookbooks/base_utils/recipes/default.rb
+++ cookbooks/base_utils/recipes/default.rb
@@ -18,7 +18,7 @@ if node['platform'] == 'debian'
   end
   
   execute "Adding dotdeb GPG-Key" do
-    command "curl -s http://www.dotdeb.org/dotdeb.gpg | apt-key add -"
+    command "cd /root && wget http://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg"
     action :run
   end
   
