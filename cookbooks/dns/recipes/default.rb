8dabc9c570f87ea071547a4b95ed6ee7e5bbd33d
Frido Koch <info@fridokoch.de>
2014-05-13 15:23:20 +0200

    Install bind

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -4,3 +4,13 @@
 #
 # Copyright 2014, Fridolin Koch, Matthias Lang
 #
+
+#we only support debian at this time
+if node['platform'] == 'debian'
+  
+  #install bind9
+  package "bind9" do
+    action :install
+  end
+  
+end
