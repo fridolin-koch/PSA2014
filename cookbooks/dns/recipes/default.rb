e8f6b6cd39ba035916edb206816d13a1807ca57e
Frido Koch <info@fridokoch.de>
2014-05-13 23:50:17 +0200

    Added basic dns

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -37,9 +37,27 @@ if node['platform'] == 'debian'
   directory "/etc/bind/zones" do
     owner "root"
     group "root"
-    mode 0700
+    mode 0755
     action :create
   end
+  
+  #our zones
+  
+  cookbook_file "/etc/bind/zones/db.psa-team1.informatik.tu-muenchen.de" do
+   source "bind_zones_db.psa-team1.informatik.tu-muenchen.de"
+   mode 0644
+   owner "root"
+   group "root"
+  end
+  
+  cookbook_file "/etc/bind/zones/db.192.168.1" do
+   source "bind_zones_db.192.168.1"
+   mode 0644
+   owner "root"
+   group "root"
+  end
+  
+  #team2
   
   service "bind9" do
     action :restart
