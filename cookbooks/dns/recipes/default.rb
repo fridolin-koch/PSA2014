10aa40ea83ff24a669ca9b7cd9c93996ddfcdfdf
Frido Koch <info@fridokoch.de>
2014-05-13 16:01:10 +0200

    Basic bind config

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -13,4 +13,36 @@ if node['platform'] == 'debian'
     action :install
   end
   
+  package "bind9utils" do
+    action :install
+  end
+  
+  #bind config files
+  
+  cookbook_file "/etc/bind/named.conf.options" do
+   source "bind_named.conf.options"
+   mode 0644
+   owner "root"
+   group "root"
+  end
+  
+  cookbook_file "/etc/bind/named.conf.local" do
+   source "bind_named.conf.local"
+   mode 0644
+   owner "root"
+   group "root"
+  end
+  
+  #create zones directory
+  directory "/etc/bind/zones" do
+    owner "root"
+    group "root"
+    mode 0700
+    action :create
+  end
+  
+  service "bind9" do
+    action :restart
+  end
+  
 end
