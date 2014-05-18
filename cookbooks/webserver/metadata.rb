b6a360bf428169c1cad3be86ca8ef1cbd0a60b68
Frido Koch <info@fridokoch.de>
2014-05-18 15:53:03 +0200

    Added basic web server cookbook

---
+++ cookbooks/webserver/metadata.rb
@@ -0,0 +1,7 @@
+name             'webserver'
+maintainer       'Fridolin Koch, Matthias Lang'
+maintainer_email 'kochfr@in.tum.de, langm@in.tum.de'
+license          'MIT'
+description      'Installs & Configures nginx+phpfpm'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
---
+++ cookbooks/webserver/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: webserver
+# Recipe:: default
+#
+# Copyright 2014, Fridolin Koch, Matthias Lang
+#
+
+#we only support debian at this time
+if node['platform'] == 'debian'
+end
