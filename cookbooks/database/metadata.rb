68d451888857a77f480975fee6f126a9c2870c18
Frido Koch <info@fridokoch.de>
2014-06-03 13:48:16 +0200

    MySQL Init

--- cookbooks/database/metadata.rb
+++ cookbooks/database/metadata.rb
@@ -5,3 +5,5 @@ license          'MIT'
 description      'Installs and configures MySQL Master and Slave'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+depends 'mysql'
--- cookbooks/database/recipes/master.rb
+++ cookbooks/database/recipes/master.rb
@@ -7,6 +7,26 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.set['mysql']['server_root_password'] = 'yolo'
+#load data bag
+mysql_secret = Chef::EncryptedDataBagItem.load_secret("/etc/chef/psa_databag_secret")
+mysql_creds = Chef::EncryptedDataBagItem.load("passwords", "mysql", mysql_secret)
+
+node.set['mysql']['server_root_password'] = mysql_creds['root_password']
 
 include_recipe 'mysql::server'
+
+#init database
+template '/etc/mysql/init.sql' do
+  source 'init.sql.erb'
+  owner 'root'
+  group 'root'
+  mode '0600'
+  variables({
+    :creds => mysql_creds
+  })
+end
+ 
+execute "Executing /etc/mysql/mysql_defaults.sql" do
+  command "mysql -u root -p#{mysql_creds['root_password']} < /etc/mysql/init.sql"
+  action :run
+end
