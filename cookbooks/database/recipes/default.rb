2b35e26131de7e20c3f94075d66bd0bdaeba6450
Frido Koch <info@fridokoch.de>
2014-06-05 13:38:20 +0200

    Added backup cronjob

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -7,7 +7,7 @@
 
 #load data bag
 mysql_secret = Chef::EncryptedDataBagItem.load_secret("/etc/chef/psa_databag_secret")
-mysql_creds = Chef::EncryptedDataBagItem.load("passwords", "mysql", mysql_secret)
+mysql_creds  = Chef::EncryptedDataBagItem.load("passwords", "mysql", mysql_secret)
 
 node.set['mysql']['server_root_password'] = mysql_creds['root_password']
 
@@ -46,4 +46,50 @@ end
 execute "Executing /etc/mysql/mysql_defaults.sql" do
   command "mysql -u root -p#{mysql_creds['root_password']} < /etc/mysql/init.sql"
   action :run
+end
+
+#backup
+if !master 
+  
+  #create backup dir
+  directory node['mysql']['backup_dir'] do
+    owner "root"
+    group "root"
+    mode 0700
+    action :create
+  end
+  
+  #cron log direcotry
+  directory '/var/log/cron' do
+    owner "root"
+    group "root"
+    mode 0755
+    action :create
+  end
+  
+  #cleanup script
+  template '/usr/local/bin/cleanup-mysql-backup' do
+    source 'cleanup_backup.erb'
+    owner 'root'
+    group 'root'
+    mode '0700'
+  end
+  
+  #backup script
+  template '/usr/local/bin/create-mysql-backup' do
+    source 'create_backup.erb'
+    owner 'root'
+    group 'root'
+    mode '0700'
+  end
+  
+  #cron script
+  cookbook_file '/etc/cron.d/mysql-backup' do
+    source 'cron_backup'
+    owner 'root'
+    group 'root'
+    mode '0644'
+  end
+  
+  
 end
