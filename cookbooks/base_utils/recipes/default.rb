d378d1e3c866239151cadc84071b626c4f1d1417
Frido Koch <info@fridokoch.de>
2014-06-24 13:24:01 +0200

    nfs common

--- cookbooks/base_utils/recipes/default.rb
+++ cookbooks/base_utils/recipes/default.rb
@@ -16,17 +16,17 @@ if node['platform'] == 'debian'
     owner "root"
     group "root"
   end
-  
+
   execute "Adding dotdeb GPG-Key" do
     command "cd /root && wget -nc http://www.dotdeb.org/dotdeb.gpg && apt-key add dotdeb.gpg"
     action :run
   end
-  
+
   execute "update apt sources" do
     command "aptitude update"
     action :run
   end
-  
+
   #install utils
   #git
   package "git" do
@@ -82,7 +82,11 @@ if node['platform'] == 'debian'
   package "lynx" do
     action :install
   end
-  
+  #nfs
+  package "nfs-common" do
+      action :install
+  end
+
   #ssh server banner
   template "/etc/motd" do
     source "sshd_banner.erb"
@@ -90,7 +94,7 @@ if node['platform'] == 'debian'
     owner "root"
     group "root"
   end
-  
+
   #ssh server configuration
   cookbook_file "/etc/ssh/sshd_config" do
     source "sshd_config"
@@ -104,14 +108,14 @@ if node['platform'] == 'debian'
     action :restart
   end
 
-  #ntp 
+  #ntp
   cookbook_file "/etc/ntp.conf" do
     source "ntp.conf"
     mode 0644
     owner "root"
     group "root"
   end
-  
+
   service "ntp" do
     action :restart
   end
@@ -122,7 +126,7 @@ if node['platform'] == 'debian'
     mode 0644
     owner "root"
     group "root"
-  end       
+  end
 
   #set shells
   cookbook_file "/etc/shells" do
@@ -131,7 +135,7 @@ if node['platform'] == 'debian'
     owner "root"
     group "root"
   end
-  
+
   #create ssh directory
   directory "/root/.ssh" do
     owner "root"
@@ -139,30 +143,30 @@ if node['platform'] == 'debian'
     mode 0700
     action :create
   end
-  
+
   cookbook_file "/root/.ssh/authorized_keys" do
     source "root_authorized_keys"
     mode 0600
     owner "root"
     group "root"
   end
-  
+
   #create users and groups
   group "psateam" do
     gid 1005
     action :create
   end
-  
+
   #get psa team members
   psateam = data_bag_item('users', 'psateam')
-  
+
   if psateam["users"]
-    
+
     psateam["users"].each do |user|
-    
+
       #only set the password once
       setPassword = !node["etc"]["passwd"].has_key?(user["name"])
-      
+
       #create user
       user user["name"] do
         supports :manage_home => true
@@ -171,7 +175,7 @@ if node['platform'] == 'debian'
         home "/home/#{user['name']}"
         shell "/bin/bash"
       end
-      
+
       if setPassword
         #change password and force the user to change it
         execute "Setting password for user #{user['name']}" do
@@ -179,16 +183,16 @@ if node['platform'] == 'debian'
             action :run
         end
       end
-      
+
     end
   end
-  
+
   #-----------
   # Networking
   #-----------
-  
+
   if node['networking']['gateway']
-  
+
     cookbook_file "/etc/firewall.rules" do
       source "firewall.rules"
       mode 0644
@@ -215,26 +219,26 @@ if node['platform'] == 'debian'
       owner "root"
       group "root"
     end
-  
+
     cookbook_file "/etc/apt/apt.conf.d/70debconf" do
      source "70debconf"
      mode 0644
      owner "root"
      group "root"
     end
-    
+
     #dhcp client config for eth0
-    
+
     cookbook_file "/etc/dhcp/dhclient.conf" do
      source "etc_dhcp_dhclient.conf"
      mode 0644
      owner "root"
      group "root"
     end
-    
-  
+
+
   end
-  
+
   #proxy
   cookbook_file "/etc/environment" do
    source "etc_environment"
@@ -242,7 +246,7 @@ if node['platform'] == 'debian'
    owner "root"
    group "root"
   end
-  
+
   #sysctl conf
   template "/etc/sysctl.conf" do
      source "etc_sysctl.erb"
@@ -258,10 +262,10 @@ if node['platform'] == 'debian'
     owner "root"
     group "root"
   end
-  
+
   execute "Restart network" do
     command "ifdown eth1 && ifup eth1"
     action :run
   end
-  
+
 end
