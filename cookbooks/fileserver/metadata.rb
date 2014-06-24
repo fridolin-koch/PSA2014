716664e357ec96425258f536bfedc60ac3899277
Frido Koch <info@fridokoch.de>
2014-06-24 12:19:23 +0200

    fileserver cookbook

---
+++ cookbooks/fileserver/metadata.rb
@@ -0,0 +1,7 @@
+name             'fileserver'
+maintainer       'Fridolin Koch, Matthias Lang'
+maintainer_email 'kochfr@in.tum.de, langm@in.tum.de'
+license          'MIT'
+description      'Installs/Configures fileserver'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.0.0'
---
+++ cookbooks/fileserver/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: fileserver
+# Recipe:: default
+#
+# Copyright 2014, Fridolin Koch, Matthias Lang
+#
+# All rights reserved - Do Not Redistribute
+#
