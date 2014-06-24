2f27899dfc8f777953634edb6de027a24e5f8031
Frido Koch <info@fridokoch.de>
2014-06-24 13:24:42 +0200

    Update all servers with one script

---
+++ chefclientall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ssh vmpsateam01-01 "chef-client"
+ssh vmpsateam01-02 "chef-client"
+ssh vmpsateam01-03 "chef-client"
+ssh vmpsateam01-04 "chef-client"
+ssh vmpsateam01-05 "chef-client"
+ssh vmpsateam01-06 "chef-client"
+ssh vmpsateam01-07 "chef-client"
