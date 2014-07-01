4a26bac609ead4d2627988286ec7cdc2a1b355fe
Matthias Lang <langm@in.tum.de>
2014-07-01 15:02:16 +0200

    Bulk adding LDAP users from benutzerdaten.csv

---
+++ cookbooks/ldap/files/default/createUser.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sed '1,1d' benutzerdaten.csv > data.csv
+INPUT=data.csv
+OLDIFS=$IFS
+IFS=","
+PORT="389"
+ROOTDN="cn=root"
+PASSWD="psa2014"
+LDIF="/tmp/createuser.ldif"
+i="1100"
+[ ! -f $INPUT ] &while read Name Vorname Geschlecht Geburtsdatum Geburtsort Nat Strasse PLZ Ort Telefon Matrikelnummer
+do
+	((i++))
+        echo "dn: uid=$Name,ou=User,dc=team01,dc=psa,dc=rbg,dc?tum,dc=de" > $LDIF
+        echo "cn: $Name $Vorname" >> $LDIF
+        echo "uid: $Name" >> $LDIF
+        echo "uidNumber: $i" >> $LDIF
+        echo "gidNumber: $i" >> $LDIF
+        echo "homeDirectory: /home/$Name" >> $LDIF
+        echo "loginShell: /bin/bash" >> $LDIF
+        echo "userPassword: $PASSWD" >> $LDIF
+        echo "objectClass: top" >> $LDIF
+        echo "objectClass: posixAccount" >> $LDIF
+
+        echo "/usr/bin/ldapmodify -x -p $PORT -D "$ROOTDN" -w $PASSWD -a -f $LDIF"
+	
+done < $INPUT
+IFS=$OLDIFS
