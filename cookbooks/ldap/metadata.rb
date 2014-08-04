8b4683d9d73375a9cef00ad66b2a9c4cfaf663b1
Frido Koch <info@fridokoch.de>
2014-08-04 15:34:00 +0200

    Removed unused readme

--- cookbooks/ldap/metadata.rb
+++ cookbooks/ldap/metadata.rb
@@ -3,5 +3,5 @@ maintainer       'Fridolin Koch, Matthias Lang'
 maintainer_email 'kochfr@in.tum.de, langm@in.tum.de'
 license          'All rights reserved'
 description      'Installs/Configures ldap'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+long_description 'N/A'
+version          '1.0.0'
